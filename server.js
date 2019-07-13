2a5b86a2773a450a128740738f05e03c81cff3af
Paul Thompson <paul.thompson@canva.com>
2019-07-13 11:59:30 +1000

    Add handlebars

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 var cons = require('consolidate');
 const router = express.Router();
+var exphbs  = require('express-handlebars');
 
 router.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/views/index.html'));
@@ -20,13 +21,19 @@ router.get('/history', function(req,res){
     res.sendfile(path.join(__dirname+'/views/history.html'));
 });
 
+router.get('/dashboard', function(req, res){
+    res.render('dashboard', {"foo": 23})
+})
+
 
 app.use('/', router);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
+app.engine('handlebars', exphbs());
 app.set('view engine', 'html');
+app.set('view engine', 'handlebars');
 
 const server = app.listen(7000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
